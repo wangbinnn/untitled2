da54d60df89fd9291c636372837411ee2cb55920
WBin <11207239+WangBin8888@user.noreply.gitee.com>
2022-06-21 02:51:21 +0000

    update src/main/java/Main.java.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("commit 66");
 System.out.println("commit github 77");
 System.out.println("commit gitee 88");
+System.out.println("commit gitee 99");
     }
 }
