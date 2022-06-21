1acf7fa9229d691c6152d6b5d161c63d202fcd92
WBin <WBin@888.com>
2022-06-21 10:24:37 +0800

    master commit 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("commit 33");
         System.out.println("commit 44");
         System.out.println("commit 55");
+        System.out.println("commit 66");
     }
 }
